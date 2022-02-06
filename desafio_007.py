bb49883008d70f4393c66e45fd2c3eceafc48cb6
Mila <millarodrigues304@gmail.com>
2022-02-06 21:17:20 +0000

    Exercicios de python
    vários deles...

---
+++ desafio_007.py
@@ -0,0 +1,5 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média.
+nota1 = float(input('digite um número: '))
+nota2 = float(input('digite outro número '))
+media = (nota1+nota2) / 2
+print(f'a media entre {nota1} e {nota2} é {media}')
---
+++ desafio_008.py
@@ -0,0 +1,10 @@
+#Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+medida = float(input('uma distância em metros: '))
+cm = medida*100
+mm = medida*1000
+print(f'a medida de {medida} corresponde a {cm}cm e {mm}mm')
+#2
+medida = int(input('uma distância em metros: '))
+cm = medida*100
+mm = medida*1000
+print(f'a medida de {medida} corresponde a {cm}cm e {mm}mm')
