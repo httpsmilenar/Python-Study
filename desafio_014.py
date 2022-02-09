b6894ee672b6b659727450ba91632cd71b437849
Mila <millarodrigues304@gmail.com>
2022-02-09 19:25:17 +0000

    exercicios 14 e 15 adicionados

---
+++ desafio_014.py
@@ -0,0 +1,4 @@
+#Ecreva um programa que converta uma temperatura digitada em °C e converta para °F:
+celsius = float(input('informe a temperatura em °C: '))
+fahrenheit = (9*celsius/5) + 32
+print(f'a temperatura de {celsius}°c corresponde a {fahrenheit}°f ')
