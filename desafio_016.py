8b5262a3bc3f63b661bebed2421f8caa59c7a39c
Mila <millarodrigues304@gmail.com>
2022-02-10 21:50:35 -0300

    exercicio de python num 16

---
+++ desafio_016.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia um número REAL qualquer pelo teclado e mostre na tela a sua porção inteira.
+import math
+número = float(input('digite um valor: '))
+print(f'o valor digitaado foi {número} e sua porção inteira é {math.trunc(número)}')
+
+número = int(input('digite um valor: '))
+print(f'o valor digitaado foi {número} e sua porção inteira é {math.sqrt(número)}')
