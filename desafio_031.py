d1ecad3cc9c9ca184f47e8ba8e44e4a06c75c5ab
Mila <millarodrigues304@gmail.com>
2022-02-16 18:33:25 -0300

    exercicio de python num 31

---
+++ desafio_031.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia um ano qualquer e diga se ele é BISSEXTO.
+ano = int(input('que ano quer analisar? '))
+if ano % 4 == 0:
+    print(f'o ano {ano} é BISSEXTO.')
+else:
+    print(f'o ano {ano} NÃO é BISSEXTO.')
