87725d26bae99fbdda8ec840baad8afc3d9640b6
Mila <millarodrigues304@gmail.com>
2022-02-16 18:11:11 -0300

    exercicio de python num 29

---
+++ desafio_029.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou IMPAR.
+numero = int(input('digite um número qualquer: '))
+resultado = numero % 2
+if resultado == 0:
+    print(f'o número {numero} é PAR.')
+else:
+    print(f'o n´mero {numero} é ÍMPAR.')
