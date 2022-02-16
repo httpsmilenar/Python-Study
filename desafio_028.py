c1323ec0b1e49ef5ba33b664965c205ac6d05754
Mila <millarodrigues304@gmail.com>
2022-02-16 18:02:32 -0300

    exercicio de python num28

---
+++ desafio_028.py
@@ -0,0 +1,7 @@
+#Escreva um programa que leia a velocidade de um carro. Se ultrapassar 80km/h, escreva uma mensagem dizendo que ele foi MULTADO. A multa vai custar R$7,00 por km acima do limite. 
+velocidade = float(input('qual é a velocidade atual do carro? '))
+if velocidade > 80:
+    print('MULTADO! você atingiu o limite permitido, que é de 80km/h.')
+    multa = (velocidade-80) * 7
+    print(f'você deve pagar uma multa no valor de R${multa:.2f}')
+print('tenha um bom dia! dirija com segurança.')
