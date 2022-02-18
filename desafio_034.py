da6d5adcfe62cffae9cb39520f89df26b17e75b6
Mila <millarodrigues304@gmail.com>
2022-02-17 21:27:04 -0300

    exercico de python num 34

---
+++ desafio_034.py
@@ -0,0 +1,11 @@
+#Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+print('=-'* 20)
+print('Analisador de Triângulos')
+print('-=' * 20)
+r1 = float(input('primeiro segmento: '))
+r2 = float(input('segundo segmento: '))
+r3 = float(input('terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('os segmentos acima PODEM formar um triângulo.')
+else:
+    print('os segmentos acima NÃO PODEM formar um triângulo. ')
