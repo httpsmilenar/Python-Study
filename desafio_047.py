f054917337c5e5a51625d2562c9b08723101f7a4
Mila <millarodrigues304@gmail.com>
2022-03-08 17:28:34 -0300

    exercico de python num 47

---
+++ desafio_047.py
@@ -0,0 +1,5 @@
+#Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+for n in range(2, 51, 2):
+    print('.', end='')
+    print(n, end=' ')
+print('acabou')
