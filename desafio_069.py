a437f07ab4b96e493dea2e661795e5241cc982ad
Mila <millarodrigues304@gmail.com>
2022-03-10 19:24:58 -0300

    exercicio de python num 69

---
+++ desafio_069.py
@@ -0,0 +1,21 @@
+#Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada, o programa deverá perguntar se o usuário quer ou não continuar. No final, mostre: A) quantas pessoas tem mais de 18 anos, B) quantos homens foram cadastrados e C) quantas mulheres tem menos de 20 anos.
+tot18 = totH = totM20 = 0
+while True:
+    idade = int(input('idade: '))
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('sexo: [M/F] ')).strip().upper()[0]
+    if idade >= 18:
+        tot18 += 1
+    if sexo == 'M':
+        totH += 1
+    if sexo == 'F' and idade < 20:
+        totM20 += 1
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print(f'total de pessoas com mais de 18 anos: {tot18}')
+print(f'ao todo temos {totH} homens cadastrados.')
+print(f'e temos {totM20} mulheres com menos de 20 anos.')
