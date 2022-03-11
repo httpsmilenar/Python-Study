9a6d9d1da38374f555ec8536406d0cba4456e6c2
Mila <millarodrigues304@gmail.com>
2022-03-11 19:10:03 -0300

    exercicio de python num 79

---
+++ desafio_079.py
@@ -0,0 +1,15 @@
+#Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente. 
+numeros = list()
+while True:
+    n = int(input('digite um valor: '))
+    if n not in numeros:
+        numeros.append(n)
+        print('valor adicionado com sucesso...')
+    else:
+        print('valor duplicado! não vou adicionar...')
+    r = str(input('quer continuar? [S/N] '))
+    if r in 'Nn':
+        break
+print('-=' * 30)
+numeros.sort()
+print(f'você digitou os valores {numeros}')
